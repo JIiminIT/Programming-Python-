0a7c5e332d8fcaf57e52f16228283a494cd77776
JiminIT <yhhs7741@naver.com>
2019-09-09 09:01:27 +0900

    add mathmodule

---
+++ Sh/모듈/mathmodule.py
@@ -0,0 +1,40 @@
+import math
+print('파이상수')
+print('math.pi :', math.pi)
+print()
+
+print('e 상수')
+print('math.e :', math.e )
+print()
+
+print('올림 - 3.5 올림')
+print('math.ceil(3.5) : ', math.ceil(3.5))
+print()
+
+print('내림 - 3.5의 내림')
+print('math.floor :', math.floor(3.5))
+print()
+
+print('계승 구하기 - 5의 (5계승)')
+print('math.factorial :', math.factorial(5))
+print()
+
+print('최대공약수 구하기 - 28과 70의 최대공약수')
+print('math.gcd(28,70) :', math.gcd(28,70))
+print()
+
+print('거듭제곱 구하기 - 2의 10승 (2^10)')
+print('math.pow(2,10) :', math.pow(2,10))
+print()
+
+print('삼각함수 - sin(1/2 b)')
+print('math.sin(math.pi) :', math.sin(math.pi/2))
+print()
+
+print('삼각함수 - cos(ㅠ)')
+print('math.cos(math.pi) :', math.cos(math.pi))
+print()
+
+print('삼각함수 - tan(0)')
+print('math.tan(0) :', math.tan(0))
+print()
