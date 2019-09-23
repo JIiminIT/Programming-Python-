706623a4b70a9ceeed3fb6b46d021fe6a04e2e4c
JiminIT <yhhs7741@naver.com>
2019-09-23 14:32:11 +0900

    에러발생

---
+++ 방과후/VIA_예외처리/my_error.py
@@ -0,0 +1,15 @@
+class Error(Exception):
+    def __init__(self, massage = "홀수 ㄴ"):
+        self.massage = massage
+
+    def __str__(self):
+        return self.massage
+
+n = 11
+try:
+    if n%2 != 0:
+        raise OddError
+    else:
+        print("짝수에요. 짝짝짝")
+except OddError as e:
+    print(e)
