f5fb7aca985588ab7c4a0ed904fd1c938d999cac
JiminIT <yhhs7741@naver.com>
2019-11-04 09:24:55 +0900

    TicTacToe Game_Manager 콘솔

--- Sh/game/Game_Manager.py
+++ Sh/game/Game_Manager.py
@@ -4,9 +4,24 @@ class GameManager:
     def __int__(self):
         self.ttt = TicTacToe()
 
-    def play(self):
+def play(self):
+    print(self.ttt)
+    while True:
         self.ttt(1,1)
         print(self.ttt)
+        row = int(input('row : '))
+        col = int(input('col : '))
+        self.ttt.set(row,col)
+        print(self.ttt)
+        if self.ttt.check_winner() =='o':
+            print("o win !!")
+            break
+        elif self.ttt_winner() == 'X':
+            print("x !!")
+            break
+        elif self.ttt_winner() == 'd':
+            print("무승부")
+            break
 
 
 if __name__ == ' __main__':
