e322bb3f2ac44897d2c0ed33cab666afefa75d2f
JiminIT <yhhs7741@naver.com>
2019-11-25 09:23:53 +0900

    web Crawling - 유미의 세포들 제목 가져오기

---
+++ 수행2/ix_프로젝트/web_crawling.py
@@ -0,0 +1,16 @@
+#pip install beautifulsoup4
+#pip install lxml
+
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+if __name__ == '__main__':
+
+    data = urlopen('https://comic.naver.com/webtoon/list.nhn?titleId=651673&weekday=wed')
+    soup = BeautifulSoup(data,'lxml')
+    #print(soup)
+
+    cartoon_titles = soup.find_all('td', attrs = {'class' : 'title'}) #td에 class title 속성을 찾음
+    for cartoon_titles in cartoon_titles:
+        title = cartoon_titles.find('a').text
+        print(title)
