55489593328208be6a909cc52c01098421053e91
JiminIT <yhhs7741@naver.com>
2019-12-05 10:11:01 +0900

    pyautoGUI

---
+++ Sh/ix_프로젝트/mau.py
@@ -0,0 +1,10 @@
+import pyautogui as pag
+
+if __name__ == '__main__':
+    x,y = pag.position(389,679,duration =2 )
+   # pag.click()
+   # pag.click()
+   #  pag.move(100,200,duration=1)
+   #  pag.rightClick()
+   #  pag.click(button="right")
+   #pag.drag(0,200, duration= 2)
