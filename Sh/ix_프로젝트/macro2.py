3dc7d11b40e17cae073d36405d5c2a26e08d9c6a
JiminIT <yhhs7741@naver.com>
2019-12-09 08:42:22 +0900

    macro pyautogui

---
+++ Sh/ix_프로젝트/macro2.py
@@ -0,0 +1,10 @@
+import pyautogui as pag
+import time
+if __name__ == '__main__':
+    pag.moveTo(200,200,2)
+    pag.click()
+    pag.typerwite("sdlkjfdslkfdjsflkdsjfslkdjsf", interval = 0.5)
+    pag.press("enter")
+    pag.typewrite("happy birth dat to namkyeoung")
+    pag.press("hangul")
+    pag.typewrite("skaruddk toddlf cnrgkgo!!!")
