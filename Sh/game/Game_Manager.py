023df265ad03f8f01dbbcc48913684eda235b8b4
JiminIT <yhhs7741@naver.com>
2019-12-23 12:10:00 +0900

    Re

--- Sh/game/Game_Manager.py
+++ Sh/game/Game_Manager.py
@@ -1,81 +1,34 @@
-from tictactoc import TicTacToe
+
+
+from tictactoe import TicTacToe
+import tkinter
+from tkinter import messagebox
 import math
 
 
 class GameManager:
-    def __int__(self):
-        self.ttt = TicTacToe()
-
-def play(self):
-    print(self.ttt)
-    while True:
-        self.ttt(1,1)
-        print(self.ttt)
-        row = int(input('row : '))
-        col = int(input('col : '))
-        self.ttt.set(row,col)
-        print(self.ttt)
-        if self.ttt.check_winner() =='o':
-            print("o win !!")
-            break
-        elif self.ttt_winner() == 'X':
-            print("x !!")
-            break
-        elif self.ttt_winner() == 'd':
-            print("무승부")
-            break
-
-
-if __name__ == ' __main__':
-    gm = GameManager()
-    gm.play()
-
-
-class Game_Manager_GUI:
     def __init__(self):
         self.ttt = TicTacToe()
-        CANVAS_SIZE = 100
-        self.TITLE_SIZE = CANVAS_SIZE /3
-        print(set.ttt)
-
-        self.root = tkinter.Tk()
-        self.root =('틱택토')
-        self.root.geometry(str(CANVAS_SIZE) + 'x'+str)
-
-        self.images = dict()
-        self.images['o'] = tkinter.PhotoImage(file = 'img/o.git')
-
-        self.canvas,bind("<Button-li", self.clike_Handiler) #버튼을 누르면 clike handiler 가 실행된다.
-
-        if self.ttt.check.winner() == "o":
-            messagebox.sgiwubfi("d아러 아아나ㅓ렁ㅇ나","o win")
-
-        elif self.ttt.check_winner() == "X":
-            messagebox.sgiwubfi("d아러 아아나ㅓ렁ㅇ나", "o win")
-
-        def draw_board(self):
-            ackear
-            self..canvas.delete('all')
-
-            x =0
-            y =0
-            for i, y in enmerate(self.ttt.board):
-                if v -- :,
-;               pass
-                elif v = "o":
-    self.cabvas,create_image(x,y, ancgir:bwdlfjk)
-
-    if i % 3 ==:
-        x=0
-        y +=self.TITLE_SIZE
-
-
-    def click_handerlier(self, event):
-        pass
 
     def play(self):
-        pass
-
-if __name__ == '__main__':
-    gm.GameManager_GUI()
-    gm.play()
+        # 반복하자
+        # show_board()
+        print(self.ttt)
+        while True:
+            #  위치를 입력받자
+            row = int(input("row: "))
+            col = int(input("col: "))
+            #  말을 놓자
+            self.ttt.set(row, col)
+            print(self.ttt)
+            #  check_winner()
+            # 결과 출력하자
+            if self.ttt.check_winner() == "O":
+                print("O 승리, ㅊㅋㅊㅋ")
+                break
+            elif self.ttt.check_winner() == "X":
+                print("X 승리, ㅊㅋㅊㅋ")
+                break
+            elif self.ttt.check_winner() == "d":
+                print("무승부")
+                break
--- Sh/game/TicTacToe.py
+++ Sh/game/TicTacToe.py
@@ -1,44 +1,61 @@
+
+
 class TicTacToe:
-    def __int__(self):
-        self.board = [",",",",",",",",",",",",",",",",","]
-        self.current_turn = "X"
+    def __init__(self):
+        self.board = [".", ".", ".", ".", ".", ".", ".", ".", "."]
+        self.current_turn = "O"
 
-    def set(self,row,col):
-        if self.get(row,col) == ".":
-
-        if self.current_turn == "0"
-            self.current_turn = "X"
+    def set(self, row, col):
+        if self.get(row, col) == ".":  # 빈칸일 경우에만 말을 놓을 수 있도록하자
+            # if self.current_turn == "O":
+            #     self.current_turn = "X"
+            # else:
+            #     self.current_turn = "O"
+            self.current_turn = "X" if self.current_turn == "O" else "O"
+            self.board[(row * 3) + col] = self.current_turn
         else:
-            self.current_turn = "O"
-        self.board[(row * 3) + col] = self.current_turn
+            print("빈칸이 아니에요. 제대로 잘 입력해요.")
 
     def get(self, row, col):
-        return self.board[(row*3) + col]
+        return self.board[(row * 3) + col]
 
     def check_winner(self):
         check = self.current_turn
         for i in range(3):
-            if self.get(i,0) == self.get(i,1) == self.get(i,2) == check:
-                # 수직
+            # -
+            if self.get(i, 0) == self.get(i, 1) == self.get(i, 2) == check:
                 return check
-                # 수평
-            elif self.get(0,i) == self.get(1,i) == self.get(2,i) == check:
+            # |
+            if self.get(0, i) == self.get(1, i) == self.get(2, i) == check:
                 return check
 
-        if self.get(0,0) == self.geeet(1,i) == self.get(2,2) == check:
+        # \
+        if self.get(0, 0) == self.get(1, 1) == self.get(2, 2) == check:
             return check
-        elif self.get(0,2) == self.get(1,1) == self.get(2,0) == check:
+        # /
+        if self.get(0, 2) == self.get(1, 1) == self.get(2, 0) == check:
             return check
 
+        # 무승부
         if not "." in self.board:
             return "d"
 
     def __str__(self):
         s = ""
-
-        for i, in enumerate(self.board):
-            s += v
+        for i, ch in enumerate(self.board):
+            s += ch
             if i % 3 == 2:
-                s+= "\n"
+                s += "\n"
+        return s
 
-        return None
+
+if __name__ == '__main__':
+    ttt = TicTacToe()
+    print(ttt)
+    ttt.set(0, 0)
+    ttt.set(0, 1)
+    ttt.set(1, 0)
+    ttt.set(1, 1)
+    ttt.set(2, 0)
+    ttt.set(2, 2)
+    print(ttt)
